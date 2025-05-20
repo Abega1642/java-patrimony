6a3e2a582ad66dcf60460ad3557f97dbca43062a
Abega1642 <hei.abega.2@gmail.com>
2025-05-20 12:23:02 +0300

    feat: add exceptions

---
+++ src/main/java/dev/razafindratelo/patrimony/exception/NotEnoughResourceQuantityException.java
@@ -0,0 +1,7 @@
+package dev.razafindratelo.patrimony.exception;
+
+public class NotEnoughResourceQuantityException extends RuntimeException {
+    public NotEnoughResourceQuantityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/dev/razafindratelo/patrimony/exception/NotImplemented.java
@@ -0,0 +1,7 @@
+package dev.razafindratelo.patrimony.exception;
+
+public class NotImplemented extends RuntimeException {
+    public NotImplemented(String message) {
+        super(message);
+    }
+}
